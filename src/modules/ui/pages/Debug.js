0fc35ae551cd54476c6717d76bce0ff1e9d01092
Greg Whatley <devgregw@outlook.com>
2018-06-21 21:08:36 -0500

    Debug script: fix broken arrays in Firebase

--- src/modules/ui/pages/Debug.js
+++ src/modules/ui/pages/Debug.js
@@ -8,33 +8,15 @@ export default class Debug extends React.Component {
         var additional = []
         return new Promise((resolve, reject) => {
             Promise.all([
-                firebase.database().ref('/tabs').once('value').then(tabsSnapshot => {
-                    let tabsDb = tabsSnapshot.val()
-                    for (var tabId in tabsDb) {
-                        var tab = tabsDb[tabId]
-                        let elements = tab.elements
-                        let headerName = tab.header
-                        tab.header = {name: headerName, width: 100, height: 100}
-                        elements.forEach(element => {
-                            if (element.type === "image")
-                                element.image = {name: element.image, width: 100, height: 100}
-                        })
-                        tab.elements = elements
-                        additional.push(firebase.database().ref(`/tabs/${tab.id}`).set(tab))
-                    }
-                }),
-                firebase.database().ref('/events').once('value').then(eventsSnapshot => {
-                    let eventsDb = eventsSnapshot.val()
-                    for (var eventId in eventsDb) {
-                        var event = eventsDb[eventId]
-                        event.header = {name: event.header, width: 100, height: 100}
-                        additional.push(firebase.database().ref(`/events/${eventId}`).set(event))
-                    }
-                }),
-                firebase.database().ref('/appearance/events').once('value').then(appSnapshot => {
-                    let eapp = appSnapshot.val()
-                    eapp.header = {name: eapp.header, width: 100, height: 100}
-                    additional.push(firebase.database().ref(`/appearance/events`).set(eapp))
+                firebase.database().ref('/tabs/ME6HV83IM0').once('value').then(snapshot => {
+                    let tab = snapshot.val()
+                    var newElements = []
+                    for (var i in tab.elements)
+                        newElements.push(tab.elements[i])
+                        alert(JSON.stringify(tab.elements))
+                        alert(JSON.stringify(newElements))
+                    tab.elements = newElements
+                    additional.push(firebase.database().ref('/tabs/ME6HV83IM0').update(tab))
                 })
             ]).then(() => Promise.all(additional).then(() => resolve()))
         })
