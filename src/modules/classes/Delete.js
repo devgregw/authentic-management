e54063b5c7f4ce32368f72c98d312e0c881cf490
Greg Whatley <devgregw@outlook.com>
2018-06-21 21:05:46 -0500

    Fixed element deletion issues

--- src/modules/classes/Delete.js
+++ src/modules/classes/Delete.js
@@ -37,14 +37,15 @@ export default class Delete {
     static element(data, index, parent) {
         var updated = parent
         updated.elements[index] = null
+        updated.elements = updated.elements.filter(o => Boolean(o))
         var promises = [
             firebase
                 .database()
                 .ref(`/tabs/${updated.id}/`)
-                .update(updated.filter(o => Boolean(o)))
+                .update(updated)
         ]
         if (data.type === 'image') 
-            promises.push(firebase.storage().ref(data.image).delete())
+            promises.push(firebase.storage().ref(data.image.name).delete())
         return Promise.all(promises)
     }
 }
