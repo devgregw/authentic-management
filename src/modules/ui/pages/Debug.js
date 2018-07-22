f53956b00850992df3fc381c54c20291c1062b92
Greg Whatley <devgregw@outlook.com>
2018-07-21 20:28:17 -0500

    New debug script

--- src/modules/ui/pages/Debug.js
+++ src/modules/ui/pages/Debug.js
@@ -6,33 +6,93 @@ import Utils from '../../classes/Utils'
 export default class Debug extends React.Component {
     run() {
         var additional = []
-        return new Promise((resolve, reject) => {
-            Promise.all([
-                firebase.database().ref('/tabs/ME6HV83IM0').once('value').then(snapshot => {
-                    let tab = snapshot.val()
-                    var newElements = []
-                    for (var i in tab.elements)
-                        newElements.push(tab.elements[i])
-                        alert(JSON.stringify(tab.elements))
-                        alert(JSON.stringify(newElements))
-                    tab.elements = newElements
-                    additional.push(firebase.database().ref('/tabs/ME6HV83IM0').update(tab))
-                })
-            ]).then(() => Promise.all(additional).then(() => resolve()))
+        return new Promise((resolve,
+            reject
+        ) => {
+            Promise
+                .all([
+                    firebase
+                        .database()
+                        .ref('/tabs/OPQ26R4SRP')
+                        .once('value')
+                        .then(snapshot => {
+                            let proc = (p,
+                                i
+                            ) => {
+                                console.log(`PROCESSING: ${p}/${i}`)
+                                if (p === 'YouTube') 
+                                    return window
+                                        .fetch(`https://www.googleapis.com/youtube/v3/search?q=${i}&maxResults=1&part=snippet&type=video&key=AIzaSyB4w3GIY9AUi6cApXAkB76vlG6K6J4d8XE`)
+                                        .then(r => r.json())
+                                        .then(json => {
+                                            if (!json || json.pageInfo.totalResults === 0) 
+                                                return null
+                                            else if (json.items[0].id.videoId !== i) 
+                                                return null
+                                            else 
+                                                return {
+                                                    title: json
+                                                        .items[0]
+                                                        .snippet
+                                                        .title,
+                                                    thumbnail: `https://img.youtube.com/vi/${i}/maxresdefault.jpg`,
+                                                    id: json
+                                                        .items[0]
+                                                        .id
+                                                        .videoId
+                                                }
+                                            })
+                                else 
+                                    return window
+                                        .fetch(`https://api.vimeo.com/videos/${i}?access_token=e2b6fedeebaa9768c909e81e2565e8a1`)
+                                        .then(r => r.json())
+                                        .then(json => {
+                                            if (!json || json.error) 
+                                                return null
+                                            else 
+                                                return {
+                                                    title: json.name,
+                                                    thumbnail: json
+                                                        .pictures
+                                                        .sizes[5]
+                                                        .link,
+                                                    id: json
+                                                        .uri
+                                                        .replace('/videos/',
+                                                            ''
+                                                        )
+                                                }
+                                            })
+                                    }
+                            let tab = snapshot.val()
+                            tab.elements.forEach((element, i) => {
+                                element.videoInfo.provider = 'YouTube'
+                                additional.push(firebase.database().ref('/tabs/OPQ26R4SRP/elements/' +
+                                i).set(element))
+                            })
+                        })
+                ])
+                .then(() => Promise.all(additional).then(() => resolve()))
         })
     }
 
     render() {
         if (!Utils.isLocalhost()) {
             alert("Unavailable")
-            window.location.replace('/')
+            window
+                .location
+                .replace('/')
             return null
         }
         if (window.confirm("Run script?")) {
-            this.run().then(() => window.location.replace('/'))
-            return <ProgressModal progressText="Working..." progressColor="warning" isOpen/>
+            this
+                .run()
+                .then(() => window.location.replace('/'))
+            return <ProgressModal progressText="Working..." progressColor="warning" isOpen="isOpen"/>
         } else {
-            window.location.replace('/')
+            window
+                .location
+                .replace('/')
             return null
         }
     }
