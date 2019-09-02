3597d448f6a3a1c10072d390fe6e0209d6dd2c4f
Greg Whatley <devgregw@outlook.com>
2019-09-02 18:37:35 -0500

    Update Debug.js

--- src/modules/ui/pages/Debug.js
+++ src/modules/ui/pages/Debug.js
@@ -9,77 +9,9 @@ export default class Debug extends React.Component {
         return new Promise((resolve,
             reject
         ) => {
+            let ids = ["1FUDXDSTKE", "5U0JPPDIPQ", "8OUVYX4DSK", "EJ4L5A34PK", "GMF632SFDS", "I7T2EPJ37B", "JNVJTB8ZP8", "NA8SKQLHIK", "PIIHMVHH1V", "SKJL92GXDY", "VX7MV7A26X", "VXEFBW4I6C"]
             Promise
-                .all([
-                    firebase
-                        .database()
-                        .ref('/tabs/3LNAQMJYKF')
-                        .once('value')
-                        .then(snapshot => {
-                            let proc = (p,
-                                i
-                            ) => {
-                                console.log(`PROCESSING: ${p}/${i}`)
-                                if (p === 'YouTube') 
-                                    return window
-                                        .fetch(`https://www.googleapis.com/youtube/v3/search?q=${i}&maxResults=1&part=snippet&type=video&key=AIzaSyB4w3GIY9AUi6cApXAkB76vlG6K6J4d8XE`)
-                                        .then(r => r.json())
-                                        .then(json => {
-                                            if (!json || json.pageInfo.totalResults === 0) 
-                                                return null
-                                            else if (json.items[0].id.videoId !== i) 
-                                                return null
-                                            else 
-                                                return {
-                                                    title: json
-                                                        .items[0]
-                                                        .snippet
-                                                        .title,
-                                                    thumbnail: json.items[0].snippet.thumbnails.high.url,
-                                                    provider: 'YouTube',
-                                                    id: json
-                                                        .items[0]
-                                                        .id
-                                                        .videoId
-                                                }
-                                        })
-                                else 
-                                    return window
-                                        .fetch(`https://api.vimeo.com/videos/${i}?access_token=e2b6fedeebaa9768c909e81e2565e8a1`)
-                                        .then(r => r.json())
-                                        .then(json => {
-                                            if (!json || json.error) 
-                                                return null
-                                            else 
-                                                return {
-                                                    title: json.name,
-                                                    thumbnail: json
-                                                        .pictures
-                                                        .sizes[5]
-                                                        .link,
-                                                    provider: 'Vimeo',
-                                                    id: json
-                                                        .uri
-                                                        .replace('/videos/',
-                                                            ''
-                                                        )
-                                                }
-                                            })
-                                    }
-                            let tab = snapshot.val()
-                            tab.elements.forEach((element, i) => {
-                                // eslint-disable-next-line
-                                if (element.type == 'video')
-                                additional.push(proc(element.provider, element.videoId).then(info => firebase.database().ref('/tabs/3LNAQMJYKF/elements/' +
-                                i).set({
-                                    id: element.id,
-                                    parent: element.parent,
-                                    type: 'video',
-                                    videoInfo: info
-                                })))
-                            })
-                        })
-                ])
+                .all(ids.map(id => firebase.database().ref().child(`/tabs/${id}`).update({specialType: 'watchPlaylist'})))
                 .then(() => Promise.all(additional).then(() => resolve()))
         })
     }
@@ -92,7 +24,7 @@ export default class Debug extends React.Component {
                 .replace('/')
             return null
         }
-        if (window.confirm("Run script?")) {
+        if (window.confirm("Convert to watchPlaylist")) {
             this
                 .run()
                 .then(() => window.location.replace('/'))
