284c9ad332bb786daf4760e4bc54386c1100cab3
Greg Whatley <me@gregwhatley.dev>
2021-05-15 19:08:44 -0500

    Update videos function

--- functions/index.js
+++ functions/index.js
@@ -28,19 +28,43 @@ exports.videos = functions.https.onRequest((req, res) => {
     const url = `https://www.youtube.com/channel/${req.query.channelId || 'UCxrYck_z50n5It7ifj1LCjA'}/videos`;
 
     request(url, { method: 'GET' }, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            var parser = new DOMParser()
-            var doc = parser.parseFromString(body, 'text/html');
-            var obj = {};
-            doc.querySelectorAll('a').forEach(a => {
-                if (a.href.indexOf('watch?v=') >= 0 && a.title) {
-                    obj[a.href.substr(9)] = a.title
+        function videos(t) {
+            let obj = {}, json = ""
+            try {
+                let parser = new DOMParser()
+                let doc = parser.parseFromString(t, "text/html")
+                let scripts = doc.querySelectorAll('script')
+                functions.logger.debug('Found ' + scripts.length + ' scripts')
+                let i = Array.from(scripts).map(s => s.innerHTML).findIndex(s => s ? s.startsWith('var ytInitialData') : false) || -1
+                if (i === -1) {
+                    functions.logger.error("Script not found")
+                    return {}
                 }
-            });
-            Object.getOwnPropertyNames(obj).forEach(n => {
-                if (!obj["livestream"] && (obj[n].toLowerCase().indexOf("stream") >= 0 || obj[n].toLowerCase().indexOf("experience") >= 0) && obj[n].toLowerCase().indexOf("postponed") === -1)
-                    obj["livestream"] = n
-            })
+                functions.logger.info('ytInitialData found at ' + i)
+                json = scripts.item(i).innerHTML.substring(20)
+                json = JSON.parse(json.substring(0, json.length - 1))
+                let arr = json.contents.twoColumnBrowseResultsRenderer.tabs[1].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].gridRenderer.items.map(i => i.gridVideoRenderer).filter(i => Boolean(i)).map(i => ({ id: i.videoId, title: i.title.runs[0].text }))
+                functions.logger.debug(arr.length + ' videos')
+                arr.forEach(v => obj[v.title] = v.id)
+            } catch (e) {
+                functions.logger.error("Error parsing videos (step 1): " + e, json || "<no json>")
+                return {}
+            }
+            try {
+                Object.getOwnPropertyNames(obj).forEach(n => {
+                    if (!obj["livestream"] && (obj[n].toLowerCase().indexOf("stream") >= 0 || obj[n].toLowerCase().indexOf("experience") >= 0) && obj[n].toLowerCase().indexOf("postponed") === -1)
+                        obj["livestream"] = n
+                })
+            } catch (e) {
+                functions.logger.error("Error parsing videos (step 2): " + e, obj)
+                return {}
+            }
+            return obj
+        }
+
+        if (!error && response.statusCode === 200) {
+            var obj = videos(body.toString())
+
             res.send(obj);
         } else
             res.send({});
